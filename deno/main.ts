656bf555b2753ade79897f174a80c559fce90337
Sami Alwazeh <alw240094@spengergasse.at>
2025-10-06 13:31:48 +0200

    syntax

--- deno/main.ts
+++ deno/main.ts
@@ -20,5 +20,5 @@ export function squareroot(a: number): number {
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Add 2 + 3 =",add(2,3));
 }
