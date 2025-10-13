cb232c4a6bce5a161480d308e499ad9d83b6f410
Sami Alwazeh <alw240094@spengergasse.at>
2025-10-13 14:03:45 +0200

    verbessern

---
+++ Primzahlen.d.ts
@@ -0,0 +1,6 @@
+export declare function primzahlen(limit: number): number[];
+export declare function primzahlenMitWurzel(limit: number): Array<{
+    primzahl: number;
+    wurzel: number;
+}>;
+//# sourceMappingURL=Primzahlen.d.ts.map
